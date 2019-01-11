240bf8cfe8e2eeb271006814dd16baf988c1d0a9
bublik009 <mykolabaydak@gmail.com>
2019-01-11 06:09:40 +0200

    getbase repaired

--- saveItem.php
+++ saveItem.php
@@ -16,7 +16,7 @@ if ($contentType === "application/json") {
 
 
     }
- 
+
     $pdodb = new PDO('mysql:host=localhost; dbname=bondardb', 'root', '');
     $pdodb -> exec("SET NAMES UTF8 ");
     $params = [ "nameeng"=>$arr['data']['name'], "Vh"=>$arr['data']['Vh'], "H"=>$arr['data']['H'], "Dv"=>$arr['data']['Dv'], "d_v"=>$arr['data']['dv'],
@@ -25,14 +25,14 @@ if ($contentType === "application/json") {
         "Ve"=>$arr['data']['V_e'], "Roh"=>$arr['data']['Roh'], "Ro"=>$arr['data']['Ro'],
         "k2"=>$arr['data']['k2'], "k4"=>$arr['data']['k4'], "L"=>$arr['data']['L'], "Fe"=>$arr['data']['Fe'], "Fet"=>$arr['data']['Fe_t'],
         "De"=>$arr['data']['De'], "Dm"=>$arr['data']['Dm'], "Delta"=>$arr['data']['Delta'],
-        "Romin"=>$arr['data']['Ro_min'], "Fmin"=>$arr['data']['Fmin'], "LopLop1"=>$arr['data']['Lop_lop1'], "c"=>$arr['data']['c'], "k"=>$arr['data']['k'],
+        "Romin"=>$arr['data']['Ro_min'], "Fmin"=>$arr['data']['Fmin'], "LopLop1"=>$arr['data']['Lop_Lop1'], "c"=>$arr['data']['c'], "k"=>$arr['data']['k'],
         "Dmin"=>$arr['data']['Dmin'], "R"=>$arr['data']['R'], "Tgbx"=>$arr['data']['TgBx'], "F2"=>$arr['data']['F2']
     ];
     $querysave = $pdodb->prepare("INSERT INTO savedeng SET nameeng=:nameeng, Vh=:Vh, H=:H, Dv=:Dv, d_v=:d_v,	Lv=:Lv, Lv2=:Lv2, Lop=:Lop, Ddv=:Ddv, Lukl=:Lukl, Deltal=:Deltal, Gv=:Gv, Ve=:Ve, Roh=:Roh, Ro=:Ro, k2=:k2, k4=:k4, L=:L, Fe=:Fe, Fet=:Fet, De=:De, Dm=:Dm, Delta=:Delta, Romin=:Romin, Fmin=:Fmin, LopLop1=:LopLop1, c=:c, k=:k, Dmin=:Dmin, R=:R, Tgbx=:Tgbx, F2=:F2");
     if($querysave -> execute($params))
     {
 
-        echo 'Yeah';
+       echo $arr['data']['Lop_Lop1'];
     }
     else{
         print_r(json_decode($_POST['json']));
